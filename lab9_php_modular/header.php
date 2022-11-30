9c3f200820b267cbbbbd04c27cdf9b18efacdc13
AbrahamLeo <abraham.leo@mhs.pelitabangsa.ac.id>
2022-11-30 13:28:36 +0700

    awal

---
+++ lab9_php_modular/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Contoh Modularisasi</title>
+    <link href="style.css" rel="stylesheet" type="text/stylesheet" media="screen" />
+</head>
+<body>
+    <nav>
+        <a href="home.php">Home</a>
+        <a href="about.php">Tentang</a>
+        <a href="https://wa.me/6282124039213">Kontak</a>
+    </nav>
+</body>
+</html>
